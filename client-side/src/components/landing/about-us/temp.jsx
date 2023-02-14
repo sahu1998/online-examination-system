918033168dfee93d9cf6858970b6183f4b8117a2
Rajani <rajani@gmail.com>
2023-02-14 12:45:26 +0530

    rajni

---
+++ client-side/src/components/landing/about-us/temp.jsx
@@ -0,0 +1,30 @@
+import { Container } from "@mui/material";
+import React from "react";
+import LandingLayout from "../../../layouts/landing-layout";
+const AboutUs = () => {
+    return (
+        <LandingLayout>
+            <Container>
+                <h2>About Us</h2>
+                <h6>About Us</h6>
+                <p>Menorah OES is a leading online source of daily news and analysis about the MenorahOES industry. We cover a wide scope. Areas of coverage include advancements in power and cooling technology, processor and server architecture, networks, storage, the colocation industry and MenorahOES company stocks, cloud, and developments in modern hyper-scale MenorahOES space, edge computing, infrastructure for machine learning, and virtual and augmented reality.</p>
+                <p>Each month, hundreds of thousands of MenorahOES professionals (C-level, business, IT, and facilities decision makers) turn to MenorahOES to help them develop MenorahOES strategies, and/or design, build, and manage world-class MenorahOESs. These buyers and decision-makers rely on MenorahOES as a trusted source of breaking news and expertise on these specialized facilities</p>
+                <h6>About Our Team</h6>
+                <p>The Menorah OES team is comprised of a dedicated group of reporters, editors, analysts, and MenorahOES industry professionals who work continuously to cover the industry’s ever-changing landscape. More on our Staff and Contributors page.</p>
+                <h6>Submit Your News</h6>
+                <p>Send news tips to our editor. Due to the high volume of inquiries, we have set up some PR Guidelines. You’ll find those on the Submit Your News page, which is also where you’ll find the right contact information</p>
+                <h6>Keep Updated Via Multiple Channels</h6>
+                <p>Stay current on our latest headlines by subscribing to our daily email updates or RSS feed.</p>
+                <h6>Social Media</h6>
+                <p>Join your colleagues on MenorahOES’s Facebook page for another way to stay updated on industry developments. Our LinkedIn Group provides another way to discuss the news and trends of the day. “Like” us on Facebook and ask for an invite to the LinkedIn Group. If cool videos of MenorahOES tours are what you seek, check out our MenorahOES video archive and the MenorahOES Videos channel on YouTube.</p>
+                <h6>MenorahOES is Part of the Knowledge & Networking Group Within Informa</h6>
+                <p>MenorahOES is a global brand within the MenorahOES Vertical of Informa’s Knowledge & Networking Group. The vertical also includes MenorahOES World conferences, the AFCOM  membership organization, and the MenorahOES Institute think tank.</p>
+                <h6>Contact Us By Mail:</h6>
+                <p>Menorah OES/Informa</p>
+                <p>101 2nd Street, Suite 350</p>
+                <p>San Francisco, CA 94105</p>
+            </Container>
+        </LandingLayout>
+    )
+}
+export default AboutUs;
---
+++ client-side/src/components/landing/syllabus/temp.jsx
@@ -0,0 +1,29 @@
+import { Container } from "@mui/material";
+import React from "react";
+import LandingLayout from "../../../layouts/landing-layout";
+const Syllabus = () => {
+    return (
+        <LandingLayout>
+            <Container>
+                <h2>Syllabus</h2>
+                <h2>CAT 2018 Syllabus</h2>
+                <p>Once you know about the CAT Pattern 2018, your next step for CAT preparation is to scan the CAT Syllabus 2018. In fact, CAT Exam Syllabus has never had any boundaries and this fact also applies to define the CAT 2018 exam syllabus. CAT 2018 exam syllabus is based on the past trend of CAT exams and putting it the right way, the CAT syllabus is the outcome of the topics which have earlier appeared in CAT exam although the type of questions, their composition, format, and number may change in each subsequent CAT exam.</p>
+                <p>
+                    The key point in CAT exam syllabus 2018 is that there is no repetition of CAT questions in any CAT exam although difficulty level of the questions in the CAT exam would not go out of CAT exam syllabus. IIM entrance exam syllabus or CAT 2018 exam Syllabus is not pre-defined by CAT 2018 convening IIM nor the CAT syllabus was outlined earlier also by the CAT exam convening IIM Bangalore, Ahmedabad, Indore, Kozhikode or Lucknow.</p>
+                <p>
+                    The key point in CAT exam syllabus 2018 is that there is no repetition of CAT questions in any CAT exam although difficulty level of the questions in the CAT exam would not go out of CAT exam syllabus. IIM entrance exam syllabus or CAT 2018 exam Syllabus is not pre-defined by CAT 2018 convening IIM nor the CAT syllabus was outlined earlier also by the CAT exam convening IIM Bangalore, Ahmedabad, Indore, Kozhikode or Lucknow.</p>
+                <h6>Sectional Sub-division of CAT 2018 Syllabus</h6>
+                <p>CAT syllabus 2018 is further subdivided. This sectional sub-division in CAT syllabus makes CAT 2018 as an exam with 5 effective sections as VARC is divided into topics on Verbal Ability and Reading Comprehension; DILR syllabus is also divided into an equal number of questions based on Data Interpretation and Logical Reasoning. CAT 2018 exam syllabus is thus based on the past paper patterns which contain questions from all these sections.</p>
+                <h6>
+                    No CAT preparation without CAT Syllabus</h6>
+                <p>The fact is that you will find it difficult to proceed with CAT preparation even if you have all the CAT preparation material unless you know the CAT exam syllabus. CAT MBA exam syllabus is much wider than one can anticipate. IIMs CAT syllabus does not leave any study topic untouched. The glimpses of CAT syllabus 2018 for MBA admission 2019-21 in IIMs and other top B-schools is a testimony to this fact.</p>
+
+                <p>If you go through the CAT exam syllabus, it will help you to decide when to begin CAT preparation? How to prepare for CAT 2018? What important topics covered in CAT syllabus need your focused attention in CAT preparation and so on so forth. Going by the past trends and past CAT exam pattern, CAT complete syllabus for each section of Common Admission Test (CAT) is shared below to help the CAT 2018 aspirants in CAT 2018 preparation in accordance with the CAT 2018 exam syllabus.</p>
+                <h6>CAT Syllabus 2018 for VARC</h6>
+                <p>Common Admission Test (CAT) exam Syllabus for VARC section in CAT 2018 is divided into 2 parts. The 1st part of CAT 2018 syllabus covers questions based on Reading Comprehension passages and the 2nd part of CAT 2018 exam syllabus on VARC is based on questions on different topics on Verbal Ability.</p>
+                <p>The past trend in CAT exam syllabus for VARC shows that there are 34 questions in this section. These questions are further divided into 24 MCQs and 10 Non-MCQs in VARC section of CAT exam. Major topics covered in CAT 2018 syllabus for VARC  and Key features of CAT syllabus for VARC are as under- </p>
+            </Container>
+        </LandingLayout>
+    )
+}
+export default Syllabus;
