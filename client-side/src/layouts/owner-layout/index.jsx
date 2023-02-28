8677df109074f500b1c6cd8bae36d880eaa2ea52
aaru <patidar4169@gmail.com>
2023-02-28 13:08:26 +0530

    sdf

--- client-side/src/layouts/owner-layout/index.jsx
+++ client-side/src/layouts/owner-layout/index.jsx
@@ -1,13 +1,20 @@
-import { Container } from '@mui/material';
-import React from 'react';
-import OwnerSideBar from '../../components/structure/owner-struct/sidenav';
+import { Container } from "@mui/material";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import OwnerSideBar from "../../components/structure/owner-struct/sidenav";
 
-export default function OwnerLayout({children}) {
+export default function OwnerLayout({ children }) {
+  const history = useNavigate();
+
+  const token = localStorage.getItem("token");
+  useEffect(() => {
+    if (token) {
+      history("/logIn");
+    }
+  }, []);
   return (
-   
     <Container maxWidth="xl">
-    <OwnerSideBar>{children}</OwnerSideBar>
+      <OwnerSideBar>{children}</OwnerSideBar>
     </Container>
-
-  )
+  );
 }
